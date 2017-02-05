1d56faea345f7675fc6dc0fa15d7b83ba7a60fe3
edbrax <ebra7152@uni.sydney.edu.au>
2017-02-05 14:20:46 +1100

    Fully functioning prototype

--- mainwindow.h
+++ mainwindow.h
@@ -2,21 +2,20 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <iostream>
-#include <fstream>
-#include <stdlib.h>
-#include <string>
+#include <QFile>
 #include <QString>
 #include <QFileDialog>
 #include <QWidget>
 #include <QMessageBox>
 
-#include "getfilecontroller.h"
+#include "outputfilecontroller.h"
+#include "datafilecontroller.h"
+#include "dataformatcontroller.h"
+
 
 namespace Ui {
 class MainWindow;
 }
-
 
 class MainWindow : public QMainWindow
 {
@@ -31,16 +30,20 @@ private slots:
     void on_getSuperFileButton_clicked();
     void on_spinQuarter_editingFinished();
     void on_spinFY_editingFinished();
+    void on_writeContributionFileButton_clicked();
+    void on_writeMemberFileButton_clicked();
 
 private:
     Ui::MainWindow *ui;
-    std::ifstream *employeeFilePtr;
-    std::ifstream *myobSuperFilePtr;
-    std::ofstream *contributionFilePtr;
-    std::ofstream *memberFilePtr;
-    std::ofstream *terminatedMemberFilePtr;
+    QFile *employeeFilePtr;
+    QFile *myobSuperFilePtr;
+    QFile *contributionFilePtr;
+    QFile *memberFilePtr;
+    QFile *terminatedMemberFilePtr;
     uint8_t financialYear;
     uint8_t quarter;
+
+    bool checkInputFilesPresent();
 };
 
 #endif // MAINWINDOW_H
